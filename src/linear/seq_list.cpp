381a8b729dd7b42d475a7980bbc51a2dc93e4d1c
CoolLoong <1542536763@qq.com>
2022-07-27 00:04:58 +0800

    添加注释

--- src/linear/seq_list.cpp
+++ src/linear/seq_list.cpp
@@ -4,15 +4,15 @@
 
 Status InitList(SqList& L) {
 	L.length = 0;
-	L.elem = new(std::nothrow) ElemType[MAXSIZE];
-	if (L.elem==nullptr) exit(RAM_OVERFLOW);
+	L.elem = new(std::nothrow) ElemType[MAXSIZE];//从elem基地址分配空间,使用std::nothrow令内存不足时new不跑出异常而是返回NULL指针
+	if (L.elem == nullptr) exit(RAM_OVERFLOW);
 	return OK;
 }
 
 Status DestroyList(SqList& L) {
 	if (L.elem != nullptr) {
 		L.length = 0;
-		delete[] L.elem;
+		delete[] L.elem;//清理空间防止内存泄露
 		L.elem = nullptr;
 		return OK;
 	}
@@ -28,7 +28,7 @@ Status ClearEmpty(SqList& L) {
 }
 
 bool ListEmpty(SqList L) {
-	return L.length == 0? true : false;
+	return L.length == 0 ? true : false;
 }
 
 int ListLength(SqList L) {
@@ -37,8 +37,8 @@ int ListLength(SqList L) {
 
 Status GetElem(SqList L, ElemType i, ElemType& e) {
 	if (L.elem != nullptr) {
-		if (i > 0 && i <= L.length) {
-			e = L.elem[i - 1];
+		if (i > 0 && i <= L.length) {//参数判断,索引范围在1---length
+			e = L.elem[i - 1];//实际存储范围在0---length-1
 			return OK;
 		}
 		else return i <= 0 ? YWM_UNDERFLOW : YWM_OVERFLOW;
@@ -48,9 +48,9 @@ Status GetElem(SqList L, ElemType i, ElemType& e) {
 
 int LocateElem(SqList L, ElemType e) {
 	if (L.elem != nullptr) {
-		for (int i = 0; i < L.length; ++i) {
-			if (L.elem[i] == e) {
-				return i + 1;
+		for (int i = 0; i < L.length; ++i) {//遍历线性表
+			if (L.elem[i] == e) {//找到第一个与e相等的元素
+				return i + 1;//实际索引范围在1---length 故需要+1
 			}
 		}
 	}
@@ -85,12 +85,12 @@ Status NextElem(SqList L, ElemType cur_e, ElemType& next_e) {
 
 Status ListInsert(SqList& L, ElemType i, ElemType e) {
 	if (L.elem != nullptr) {
-		if (L.length == MAXSIZE) return YWM_OVERFLOW;
+		if (L.length == MAXSIZE) return YWM_OVERFLOW;//满了就不能插入元素了
 		if (i<1 || i > L.length + 1) return ERROR;
-		for (int j = L.length - 1; j >= i - 1; --j) {
-			L.elem[j + 1] = L.elem[j];
+		for (int j = L.length - 1; j >= i - 1; --j) {//从后往前遍历到i-1
+			L.elem[j + 1] = L.elem[j];//每个元素都后移,将i-1的位置空出来
 		}
-		L.elem[i - 1] = e;
+		L.elem[i - 1] = e;//插入到该位置
 		L.length++;
 		return OK;
 	}
@@ -101,9 +101,10 @@ Status ListDelete(SqList& L, ElemType i) {
 	if (L.elem != nullptr) {
 		if (L.length == 0) return YWM_UNDERFLOW;
 		if (i<1 || i > L.length) return ERROR;
-		for (int j = i; j <= L.length; ++j) {
-			L.elem[j - 1] = L.elem[j];
+		for (int j = i; j <= L.length-1; ++j) {//从i遍历到列表结尾
+			L.elem[j - 1] = L.elem[j];//每个元素都左移
 		}
+		L.elem[L.length] = -1;//由于左移 最后一个元素置-1逻辑删除
 		L.length--;
 		return OK;
 	}
--- src/linear/seq_list.h
+++ src/linear/seq_list.h
@@ -1,24 +1,84 @@
 #ifndef SEQ_LIST
 #define SEQ_LIST
 #include "../common.h"
-typedef struct{
+
+typedef struct {
 	ElemType* elem;//存储空间的基地址
 	int length;//线性表长度
 }SqList;
 
+/*
+初始化线性表
+参数: SqList结构体引用
+返回值: 执行状态码
+*/
 Status InitList(SqList& L);
+/*
+销毁线性表
+参数: SqList结构体引用
+返回值: 执行状态码
+*/
 Status DestroyList(SqList& L);
+/*
+清空线性表
+参数: SqList结构体引用
+返回值: 执行状态码
+*/
 Status ClearEmpty(SqList& L);
+/*
+判断线性表是否为空
+参数: SqList结构体
+返回值: 线性表是否为空
+*/
 bool ListEmpty(SqList L);
+/*
+获取线性表的长度
+参数: SqList结构体
+返回值: 线性表的长度
+*/
 int ListLength(SqList L);
-//i的取值范围在(1-length)
+
+/*
+获取线性表指定位置的元素
+参数: SqList结构体 位置索引(1-length) 保存结果的元素引用
+返回值: 执行状态码
+*/
 Status GetElem(SqList L, ElemType i, ElemType& e);
-//返回值从1-length
+/*
+获取线性表指定元素的位置
+参数: SqList结构体 待查询的元素
+返回值: 该元素所在的索引(1-length)
+*/
 int LocateElem(SqList L, ElemType e);
+/*
+获取线性表指定元素的前驱元素
+参数: SqList结构体 待查询的元素 保存前驱元素的元素引用
+返回值: 执行状态码
+*/
 Status PriorElem(SqList L, ElemType cur_e, ElemType& pre_e);
+/*
+获取线性表指定元素的后继元素
+参数: SqList结构体 待查询的元素 保存后继元素的元素引用
+返回值: 执行状态码
+*/
 Status NextElem(SqList L, ElemType cur_e, ElemType& next_e);
-Status ListInsert(SqList& L, ElemType i,ElemType e);
+/*
+插入指定元素到指定位置
+参数: SqList结构体引用 插入元素的位置 待插入的元素
+返回值: 执行状态码
+*/
+Status ListInsert(SqList& L, ElemType i, ElemType e);
+/*
+删除指定位置的元素
+参数: SqList结构体引用 待删除元素的位置索引
+返回值: 执行状态码
+*/
 Status ListDelete(SqList& L, ElemType i);
+/*
+遍历整个线性表并输出
+参数: SqList结构体
+返回值: 执行状态码
+*/
 Status TraverseList(SqList L);
 
 #endif
